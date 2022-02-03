10f5f8acefc618d06a7e112340f4535ba9a8d7e7
Anu Sethi <anu1301@hotmail.co.uk>
2022-02-03 13:08:47 +0000

    Add quizz questions and answers to js

--- assets/js/food_and_drink.js
+++ assets/js/food_and_drink.js
@@ -7,7 +7,8 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [{
+let questions = [
+    {
         question: 'What type of pastry are profiteroles made from?',
         choice1: 'Puff pastry',
         choice2: 'Short crust pastry',
@@ -16,19 +17,75 @@ let questions = [{
         answer: 3,
     },
     {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
+        question: "What is the main flavour of aioli?",
+        choice1: "Anchovy",
+        choice2: "Ginger",
+        choice3: "Lemon",
+        choice4: "Garlic",
+        answer: 4,
+    },
+    {
+        question: "Which vegetable can be Oyster, Chestnut or Shitaki?",
+        choice1: "Mushroom",
+        choice2: "Peppers",
+        choice3: "Tomatoes",
+        choice4: "Aubergine",
+        answer: 1,
+    },
+    {
+        question: "What is Wiener Schnitzel?",
+        choice1: "Pork",
+        choice2: "Beef",
+        choice3: "Venison",
+        choice4: "Veal",
+        answer: 4,
+    },
+    {
+        question: "How is steak tartare cooked?",
+        choice1: "Barbecued",
+        choice2: "Served Raw",
+        choice3: "Baked",
+        choice4: "Fried",
+        answer: 2,
+    },
+    {
+        question: "Which drink is Worcester sauce traditionally added to?",
+        choice1: "Dirty Martini",
+        choice2: "Black Russian",
+        choice3: "Bloody Mary",
+        choice4: "Caipiroska",
         answer: 3,
     },
     {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
+        question: "Which fish is the main ingredient of Scotch Woodcock?",
+        choice1: "Anchovy",
+        choice2: "Sardines",
+        choice3: "Whitebait",
+        choice4: "Tuna",
+        answer: 1,
+    },
+    {
+        question: "What is beef fillet cooked in puff pastry called?",
+        choice1: "Beef in puff pastry",
+        choice2: "Beef Wellington",
+        choice3: "Puff pastry parcel",
+        choice4: "Beef fillet en croute",
+        answer: 2,
+    },
+    {
+        question: "Where is Red Windsor cheese from?",
+        choice1: "Windsor",
+        choice2: "Chedder",
+        choice3: "Leicestershire",
+        choice4: "Lancashire",
+        answer: 3,
+    },
+    {
+        question: "What is a Worcester Pearmain?",
+        choice1: "Pear",
+        choice2: "Plum",
+        choice3: "Cherry",
+        choice4: "Apple",
         answer: 4,
     },
 ];
@@ -47,9 +104,10 @@ startGame = () => {
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
         return window.location.assign('/end.html');
     }
-    questionCounter++;
+    questionCounter++
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
